fffc807df4f2d8af96ba1dde1c42d5c80d3ab1d2
oguz kaya <oguzkayaya@gmail.com>
2019-05-04 19:21:29 +0300

    todos listeleme sayfasi olusturuldu

---
+++ Controllers/ToDoController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using dotnetMvc.Models;
+using dotnetMvc.Data;
+
+
+namespace dotnetMvc.Controllers
+{
+  [Route("todos")]
+  public class ToDoController : Controller
+  {
+    private readonly DataContext _dataContext;
+
+    public ToDoController(DataContext dataContext)
+    {
+      _dataContext = dataContext;
+    }
+
+    [HttpGet("")]
+    public IActionResult GetToDos(){
+      var todos = _dataContext.ToDoList.ToList();
+      return Json(todos);
+    }
+
+  }
+}
